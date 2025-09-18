66c99ff804ebb0d7eb5dc72ce17ada5634ccaee8
Aaryan Patel <a52patel@uwaterloo.ca>
2025-09-17 20:57:58 -0400

    Update client.ts

--- packages/server/src/database/client.ts
+++ packages/server/src/database/client.ts
@@ -2,23 +2,28 @@ import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./types";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabasePublishableKey =
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
 
 if (!supabaseUrl) {
   throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL");
 }
 
-if (!supabaseAnonKey) {
-  throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY");
+if (!supabasePublishableKey) {
+  throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY");
 }
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    persistSession: true,
-    autoRefreshToken: true,
-    detectSessionInUrl: true,
-  },
-});
+export const supabase = createClient<Database>(
+  supabaseUrl,
+  supabasePublishableKey,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true,
+    },
+  }
+);
 
 export { createClient } from "@supabase/supabase-js";
 export type { SupabaseClient } from "@supabase/supabase-js";
