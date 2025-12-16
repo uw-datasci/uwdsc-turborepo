10a3112ee9835b7f7c1598d4b071374275a5a74e
Jocelyn Xu <joce.xu06@gmail.com>
2025-12-16 15:35:47 -0500

    fix app service lint

--- packages/server/cxc/src/services/applicationService.ts
+++ packages/server/cxc/src/services/applicationService.ts
@@ -10,7 +10,7 @@ export class ApplicationService {
   private readonly repository: ApplicationRepository;
 
   constructor(supabaseClient: SupabaseClient) {
-    this.repository = new ApplicationRepository(supabaseClient);
+    this.repository = new ApplicationRepository();
   }
 
   /**
