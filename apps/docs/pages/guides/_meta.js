28014fa2e4fae680905c430e98a08f6cf82da055
joc31yn <140670138+joc31yn@users.noreply.github.com>
2026-01-02 07:53:22 +0000

    Update docs based on merge #109

--- apps/docs/pages/guides/_meta.js
+++ apps/docs/pages/guides/_meta.js
@@ -1,6 +1,7 @@
 export default {
   "adding-components": "Adding Components",
   "creating-api": "Creating API Endpoints",
+  "teams-feature": "Teams Feature",
   database: "Database Setup",
   development: "Development Tips",
 };
