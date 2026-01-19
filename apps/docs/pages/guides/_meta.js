186a49bd9075f2b2929250a6089537f8e31f67e1
joc31yn <140670138+joc31yn@users.noreply.github.com>
2026-01-19 03:19:52 +0000

    Update docs based on merge #134

--- apps/docs/pages/guides/_meta.js
+++ apps/docs/pages/guides/_meta.js
@@ -2,6 +2,7 @@ export default {
   "adding-components": "Adding Components",
   "creating-api": "Creating API Endpoints",
   "application-review": "Application & Review System",
+  "role-management": "Role Management System",
   database: "Database Setup",
   development: "Development Tips",
 };
